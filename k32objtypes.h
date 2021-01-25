30d4dd28cd647cb7c2a14c1f2193003af6c486ab
Roy Merkel <roy_d_merkel@hotmail.com>
2021-01-24 20:46:30 -0800

    Update k32objtypes.h
    Fixed WINME_K32OBJ_ #define values based on values found experimentally.

--- k32objtypes.h
+++ k32objtypes.h
@@ -1,6 +1,7 @@
 //================================== 
 // WIN95_K32OBJ_* - Matt Pietrek 1995 
-// WIN98_K32OBJ_*, WINME_K32OBJ_* - AntÛnio Feij„o - Remote Library
+// WIN98_K32OBJ_* - Ant√≥nio Feij√£o - Remote Library
+// WINME_K32OBJ_* - Roy Merkel
 // FILE: TIB.H 
 //================================== 
 #ifndef K32_OBJ_TYPES_H
@@ -45,17 +46,19 @@
 #define WINME_K32OBJ_EVENT                0x2
 #define WINME_K32OBJ_MUTEX                0x3
 #define WINME_K32OBJ_CRITICAL_SECTION     0x4
-#define WINME_K32OBJ_CHANGE               0x5
+//#define                0x5
 #define WINME_K32OBJ_PROCESS              0x6
 #define WINME_K32OBJ_THREAD               0x7
 #define WINME_K32OBJ_FILE                 0x8
-#define WINME_K32OBJ_CONSOLE              0x9
-#define WINME_K32OBJ_SCREEN_BUFFER        0xA
-#define WINME_K32OBJ_MAILSLOT             0xB
-#define WINME_K32OBJ_SERIAL               0xC
+#define WINME_K32OBJ_CHANGE               0x9
+#define WINME_K32OBJ_CONSOLE              0xA
+//#define              0xB
+#define WINME_K32OBJ_SCREEN_BUFFER        0xC
 #define WINME_K32OBJ_MEM_MAPPED_FILE      0xD
-#define WINME_K32OBJ_PIPE                 0xE
+#define WINME_K32OBJ_SERIAL               0xE
 #define WINME_K32OBJ_DEVICE_IOCTL         0xF
-#define WINME_K32OBJ_TOOLHELP_SNAPSHOT    0x10
-#define WINME_K32OBJ_SOCKET               0x11
+#define WINME_K32OBJ_PIPE                 0x10
+#define WINME_K32OBJ_MAILSLOT             0x11
+#define WINME_K32OBJ_TOOLHELP_SNAPSHOT    0x12
+#define WINME_K32OBJ_SOCKET               0x13
 #endif
